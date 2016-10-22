e16c47c6cf92b985d0cb5e320163460115549e87
Michael Pfeifer <mikepf97@gmail.com>
2016-10-22 17:39:26 -0600

    Cleaned vote

--- Day05/vote.cpp
+++ Day05/vote.cpp
@@ -5,44 +5,38 @@
 using namespace std;
 
 int main() {
-	int times;
-	cin >> times;
-	for(int i = 0; i < times; i++) {
-		int votes;
-		cin >>votes;
-		int max = 0;
-		int nextMax = 0;
-		int maxLocation = 0;
-		int total = 0;
-		bool tied = false;
-		for(int j = 0; j < votes; j++) {
-			int temp;
-			cin >> temp;
-			total += temp;
-			if(temp > max) {
-				nextMax = max;
-				max = temp;
-				tied = false;
-				maxLocation = j;
-			}
-			else if(temp == max) {
-				tied = true;
-			}
-			else {
-			}
-		}
-		if(tied) {
-			cout << "no winner" << endl;
-		}
-		else {
-			if(max * 2 > total) {
-				cout << "majority winner ";
-			}
-			else {
-				cout << "minority winner ";
-			}
-			cout << maxLocation + 1 << endl;
-		}
-		
-	}
+    int times;
+    cin >> times;
+    for(int i = 0; i < times; i++) {
+        int votes, max = 0, nextMax = 0;
+        int maxLocation = 0, total = 0;
+        cin >> votes;
+        bool tied = false;
+        for(int j = 0; j < votes; j++) {
+            int temp;
+            cin >> temp;
+            total += temp;
+            if(temp > max) {
+                nextMax = max;
+                max = temp;
+                tied = false;
+                maxLocation = j;
+            }
+            else if(temp == max) {
+                tied = true;
+            }
+        }
+        if(tied) {
+            cout << "no winner" << endl;
+        }
+        else {
+            if(max * 2 > total) {
+                cout << "majority winner ";
+            }
+            else {
+                cout << "minority winner ";
+            }
+            cout << maxLocation + 1 << endl;
+        }
+    }
 }
